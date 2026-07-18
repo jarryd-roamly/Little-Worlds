7ffaac0e0b6943d2ed0829f7e635e20147f2a1d6
Jarryd <jarryd@roamly.biz>
2026-07-18 20:51:31 +0200

    Create companion.dart

---
+++ lib/widgets/companion.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class Companion extends StatefulWidget {
+  final Color color;
+  final double size;
+  final bool sleepy;
+  const Companion(
+      {super.key, required this.color, this.size = 110, this.sleepy = false});
+  @override
+  State<Companion> createState() => _CompanionState();
+}
+
+class _CompanionState extends State<Companion>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _c =
+      AnimationController(vsync: this, duration: const Duration(seconds: 2))
+        ..repeat(reverse: true);
+
+  @override
+  void dispose() {
+    _c.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _c,
+      builder: (_, __) {
+        final bob = sin(_c.value * pi) * (widget.sleepy ? 2 : 8);
+        final scale = widget.sleepy ? 1.0 : 1.0 + _c.value * 0.06;
+        return Transform.translate(
+          offset: Offset(0, -bob),
+          child: Transform.scale(
+            scale: scale,
+            child: Container(
+              width: widget.size,
+              height: widget.size,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: RadialGradient(colors: [
+                  widget.color.withOpacity(widget.sleepy ? .5 : 1),
+                  widget.color.withOpacity(.6),
+                ]),
+              ),
+              child: Center(
+                child: Container(
+                  width: widget.size * .4,
+                  height: widget.sleepy ? 3 : widget.size * .4,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(.6),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
