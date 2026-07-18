2e4c52941e339a0f27fc10015a7780c18cc79efb
Jarryd <jarryd@roamly.biz>
2026-07-18 20:50:25 +0200

    Create realms.dart

---
+++ lib/core/realms.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+enum GameKind { sorting, matchPairs, hiddenSearch }
+
+class RealmDef {
+  final String id;
+  final String labelKey;
+  final Color color;
+  final GameKind kind;
+  final int freeLevels;
+  final int totalLevels;
+  final bool locked;
+  const RealmDef({
+    required this.id,
+    required this.labelKey,
+    required this.color,
+    required this.kind,
+    required this.freeLevels,
+    required this.totalLevels,
+    this.locked = false,
+  });
+}
+
+const List<RealmDef> kRealms = [
+  RealmDef(
+    id: 'home',
+    labelKey: 'realmHome',
+    color: Color(0xFFAB47BC),
+    kind: GameKind.sorting,
+    freeLevels: 4,
+    totalLevels: 4,
+  ),
+  RealmDef(
+    id: 'town',
+    labelKey: 'realmTown',
+    color: Color(0xFF1E88E5),
+    kind: GameKind.matchPairs,
+    freeLevels: 1,
+    totalLevels: 6,
+    locked: true,
+  ),
+  RealmDef(
+    id: 'nature',
+    labelKey: 'realmNature',
+    color: Color(0xFF43A047),
+    kind: GameKind.hiddenSearch,
+    freeLevels: 1,
+    totalLevels: 6,
+    locked: true,
+  ),
+  RealmDef(
+    id: 'space',
+    labelKey: 'realmSpace',
+    color: Color(0xFFF9A825),
+    kind: GameKind.sorting,
+    freeLevels: 0,
+    totalLevels: 6,
+    locked: true,
+  ),
+];
